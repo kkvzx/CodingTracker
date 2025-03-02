95f386ab2d63d8844c8deb9e6ab2740d6e5db077
Konrad <konrvd.kvzx@gmail.com>
2025-03-02 09:34:16 +0100

    Clean up

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -2,26 +2,9 @@
 using CodingTracker.Presenters;
 using CodingTracker.Views;
 
-/*
- * - Configuration file,
- * - model,
- * - database/table creation,
- * - CRUD controller (where operations will happen)
- * - Table visualization engine,
- * - Validation of data
- *
- *
- * MVP - Model View Presenter
- * Model - Odpowiada za przechowywanie danych i zasad biznesowych (Data
- * View - Interfejs przsez który użytkownik wchodzi w interakcje
- * Presenter - Służy jako most między Model i View, manipulując danymi pochodzącymi z Modelu, przekazując je do View
- */
-
-/*  TODO
- *  1. Zająć się tableVisualizationEngine
- */
 CodingTrackerRepository repository = new();
 CodingSessionsView codingSessionsView = new();
 CodingSessionPresenter presenter = new(codingSessionsView, repository);
 
 presenter.Run();
+<
